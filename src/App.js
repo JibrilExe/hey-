9f729a92002dfe6fc9e0ebc4e46378b8b480e994
JibrilExe <cedric00ik@gmail.com>
2025-03-14 16:52:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import logo from './heylogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Let's get marketing</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <div>
+          <h1>Image query</h1>
+          <input type="text" id="titleInput" placeholder="Enter title here" />
+        </div>
+      </main>
     </div>
   );
 }
